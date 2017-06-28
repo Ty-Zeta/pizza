d1703724c0cb034c1e0b559b6c29ac515f91979a
Ty-Zeta <tyofzeta@gmail.com>
2017-06-28 11:12:08 -0400

    Prices on pizza sizes

--- pizza.rb
+++ pizza.rb
@@ -1,33 +1,86 @@
 def size
-  size = ["small", "medium", "large", "x-large"].sample
+  size = ["small", "medium", "large" ].sample
+
 end
 def crust
   crust = ["thin", "pan", "brooklyn"].sample
 end
 def cheese
-  cheese = ["no cheese", "light cheese", "cheese", "extra cheest", "double cheest"].sample
+  cheese = ["no cheese", "light cheese", "cheese", "extra cheese"].sample
 end
 def sauce
   sauce = ["regular sause", "marinara sause", "BBQ sause", "alfredo sause"].sample
 end
 def meat
-  meat = ["pepperoni","double pepperoni", "bacon", "ham", "no meat"].sample
+  meat = ["pepperoni", "bacon", "ham", "no meat"].sample
 end
 def veggies
   veggies = ["pineapple", "mushrooms", "peppers", "no veggies"].sample
 end
-p "How many $10 pizzas would you like?"
+p "How many pizzas would you like?"
   x = gets.chomp.to_i
-count = 0
+count = 1
+cost = 0
 x.times do
-  p "Pizza #{count+1}"
-  count +=1
-  puts size
+  pizza_size = size
+  p "Pizza #{count}"
+    count +=1
+  # puts "size is #{pizza_size}"
+  #   if pizza_size == "small"
+  #     cost += 3
+  #     puts "Total cost is #{cost}"
+  #   elsif pizza_size == "medium"
+  #     cost += 5
+  #     puts "Total cost is #{cost}"
+  #   elsif pizza_size == "large"
+  #     cost += 7
+  #     puts "Total cost is #{cost}"
+  #end
   puts crust
   puts cheese
   puts sauce
   puts meat
   puts veggies
+  puts "size is #{pizza_size}"
+    if pizza_size == "small"
+      cost += 3
+      puts "Total cost is $#{cost}"
+    elsif pizza_size == "medium"
+      cost += 5
+      puts "Total cost is $#{cost}"
+    elsif pizza_size == "large"
+      cost += 7
+      puts "Total cost is $#{cost}"
+    end
   puts
 end
-puts "Total cost is $#{'%.2f' %(x * 10.06)}"
+
+# def price(size)
+#   cost = 0
+#   # p "size is #{size}"
+#   # p "cost is #{cost}"
+#   if size == "small"
+#     cost = cost + 3
+#   elsif size == "medium"
+#     cost = cost + 5
+#   else size == "large"
+#     cost = cost + 7
+#   end
+#   cost
+# end
+
+  #puts price(size)
+  # p "size is #{size}"
+  # p "cost is #{cost}"
+#   puts "size is #{size}"
+#   if size == "small"
+#     cost = cost + 3
+#   elsif size == "medium"
+#     cost = cost + 5
+#   else size == "large"
+#     cost = cost + 7
+#   end
+# end
+# puts cost
+#puts "Total cost is $#{'%.2f' %(x * 10.06)}"
+#puts "Total cost is $#{cost}."
