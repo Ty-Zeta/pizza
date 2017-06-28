2877a50afa1cbf27becb7fab4fa57f228bf8fd27
Ty-Zeta <tyofzeta@gmail.com>
2017-06-28 11:21:08 -0400

    Different prices on sizes with tax

--- pizza.rb
+++ pizza.rb
@@ -1,6 +1,5 @@
 def size
   size = ["small", "medium", "large" ].sample
-
 end
 def crust
   crust = ["thin", "pan", "brooklyn"].sample
@@ -25,23 +24,12 @@ x.times do
   pizza_size = size
   p "Pizza #{count}"
     count +=1
-  # puts "size is #{pizza_size}"
-  #   if pizza_size == "small"
-  #     cost += 3
-  #     puts "Total cost is #{cost}"
-  #   elsif pizza_size == "medium"
-  #     cost += 5
-  #     puts "Total cost is #{cost}"
-  #   elsif pizza_size == "large"
-  #     cost += 7
-  #     puts "Total cost is #{cost}"
-  #end
+  puts pizza_size
   puts crust
   puts cheese
   puts sauce
   puts meat
   puts veggies
-  puts "size is #{pizza_size}"
     if pizza_size == "small"
       cost += 3
       puts "Total cost is $#{cost}"
@@ -54,33 +42,4 @@ x.times do
     end
   puts
 end
-
-# def price(size)
-#   cost = 0
-#   # p "size is #{size}"
-#   # p "cost is #{cost}"
-#   if size == "small"
-#     cost = cost + 3
-#   elsif size == "medium"
-#     cost = cost + 5
-#   else size == "large"
-#     cost = cost + 7
-#   end
-#   cost
-# end
-
-  #puts price(size)
-  # p "size is #{size}"
-  # p "cost is #{cost}"
-#   puts "size is #{size}"
-#   if size == "small"
-#     cost = cost + 3
-#   elsif size == "medium"
-#     cost = cost + 5
-#   else size == "large"
-#     cost = cost + 7
-#   end
-# end
-# puts cost
-#puts "Total cost is $#{'%.2f' %(x * 10.06)}"
-#puts "Total cost is $#{cost}."
+puts "Total cost is $#{'%.2f' %(cost * 1.06)}"
