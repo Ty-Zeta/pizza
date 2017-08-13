dabcdaea79c4e5115f02cf24254376e228328728
Ty-Zeta <tyofzeta@gmail.com>
2017-08-13 16:06:47 -0400

    updated with comments

--- pizza.rb
+++ pizza.rb
@@ -22,16 +22,31 @@ end
 def del()
   delivery = ["Yes", "No"].sample
 end
+
+# sets of arrays are set with .sample to have one be randomly choosen for function to call upon later
+
 y_del = del
+# variable is set as the randomly choosen element so whenever its called, I'll get the same element instead of a differently choosen one each time
+
 p "Is this for delivery?"
   puts y_del
+ # delievery decides whether or not certain charges will occur later
+
 p "How many pizzas would you like?"
   x = gets.chomp.to_i
+# user gives how many pizzas they want and that value is turned into integer
+
   puts
 count = 1
 cost = 0
+# counter is displayed so that each corresponding pizza will display what order its in as well as the total cost
+
 x.times do
+# user given number is how many times the function will run
+
   pizza_size = size
+# like delievery, size is given to another variable so I can keep the same size of each pizza and its price later one
+
   p "Pizza #{count}"
     count +=1
   puts pizza_size
@@ -40,6 +55,8 @@ x.times do
   puts sauce
   puts meat
   puts veggies
+# each of the randomly selected toppings and sizes are shown to viewer
+
     if pizza_size == "small"
       cost += 3
       puts "Total cost is $#{cost}"
@@ -52,16 +69,28 @@ x.times do
     end
   puts
 end
+# if statement determines the cost of each pizza by its size by saying if it's this size, add this value to the cost counter
+# if it's not this size, move onto the next elsif statement until it comes up true
+# at the end of each cycle, the cost of each pizza is added onto the cost counter and starts again until the number user gave is complete
+
 if y_del == "Yes"
   puts "The delivery charge is $3.00."
+# if yes was choosen from array, printed statement lets user know there is a $3 charge
+
   tip_charge = time
+# variable is set to randomly choosen time slot
+
   del_charge = 3
+# value is given to variable to have $3 charge added to final formula
+
   puts tip_charge
     elsif y_del == "No"
       tip_charge = 0
       del_charge = 0
       puts "There is no delivery charge."
 end
+# if delievery is set as no, then all the values for the tip and delievery fee is set as 0
+
       if tip_charge == "15-20 min"
         tip_charge = 7
         puts "Your tip is $#{tip_charge}."
@@ -75,6 +104,14 @@ end
         tip_charge = 0
         puts "Your tip is $#{tip_charge}."
       end
+# if statement sets conditions for how fast the delievery is done and how good of a fee is set
+# faster service means better tip, printed statement then states how much the tip is
+
 total_charge = "#{cost * 1.06 + tip_charge + del_charge}"
+# variable total charge is equal to the total cost of each size of the pizzas added together with counter cost
+# then multiplied by 1.06 to represent WV state tax, plus the tip and delievery charge that was determined by the randomly selected answer
+
 puts "Total cost is $#{'%.2f' %total_charge}"
+# User is given total cost that is then rounded down to the last two decimal places with a modulous of itself
+
 puts
